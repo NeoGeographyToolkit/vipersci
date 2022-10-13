6f94019453637a5693637ec78716a989482ccd0a
Schiewe, Evan A. (ARC-TI)[KBR Wyle Services, LLC] <evan.a.schiewe@nasa.gov>
2022-10-13 11:11:13 -0700

    Roll up of heatmap updates
    * add profile support to write_geotiff_rasterio
    * docstring cleanup
    * allow for sampling subregion of area covered by input data
    * allow for reuse of frequency data
    * use pool.imap for less data copies when multiprocessing

--- tests/carto/heatmaps/test_density_heatmap.py
+++ tests/carto/heatmaps/test_density_heatmap.py
@@ -29,6 +29,7 @@ import unittest
 
 import numpy as np
 from rasterio import transform, windows
+import shapely
 
 from vipersci.carto import heatmap as hm
 
@@ -46,20 +47,12 @@ class TestHeatmapAreaBin(unittest.TestCase):
     def test_density_heatmap_100x100_uniform(self):
         self.uniform_density_heatmap_runner((100, 100), processes=8)
 
-    def test_density_heatmap_gsd_exception(self):
-        try:
-            self.uniform_density_heatmap_runner((5, 5), gsd=10, processes=8)
-        except ValueError:
-            self.assertTrue(True)
-        else:
-            self.assertTrue(False)
-
     def test_density_heatmap_diag(self, processes: int = 1):
         shape = (10, 10)
         x = np.arange(0.5, shape[1] - 0.5, 1)
         y = np.arange(0.5, shape[0] - 0.5, 1)
         values = np.ones(shape[0] - 1)
-        affine_transform, out_freq, out_avg = hm.generate_density_heatmap(
+        affine_transform, out_freq, out_avg, _ = hm.generate_density_heatmap(
             x, y, values, gsd=1, radius=1, padding=0, processes=processes
         )
 
@@ -73,9 +66,7 @@ class TestHeatmapAreaBin(unittest.TestCase):
         self.assertTrue(np.array_equal(out_avg, expected_output))
         self.assertEqual(
             affine_transform,
-            transform.from_bounds(
-                -1, 0, shape[1] - 1, shape[0], shape[1], shape[0]
-            )
+            transform.from_bounds(-1, 0, shape[1] - 1, shape[0], shape[1], shape[0]),
         )
 
         expected_freq = np.zeros(shape, dtype=np.int32)
@@ -97,7 +88,7 @@ class TestHeatmapAreaBin(unittest.TestCase):
         y_arr = np.arange(0.5, shape[0], 1)
         x, y = np.meshgrid(x_arr, y_arr)
         values = np.ones(len(x_arr) * len(y_arr))
-        affine_transform, frequencies, out_avg = hm.generate_density_heatmap(
+        affine_transform, frequencies, out_avg, _ = hm.generate_density_heatmap(
             x.ravel(),
             y.ravel(),
             values,
@@ -122,10 +113,142 @@ class TestHeatmapAreaBin(unittest.TestCase):
         )
         self.assertEqual(
             expected_bounds,
-            windows.bounds(
-                windows.get_data_window(out_avg), affine_transform
-            )
+            windows.bounds(windows.get_data_window(out_avg), affine_transform),
         )
-        # TODO create proper frequency tests
-        self.assertLessEqual(np.amax(frequencies), 4)
-        self.assertGreaterEqual(np.amin(frequencies), 0)
+
+        expected_frequencies = np.ones((shape[0] + 1, shape[1] + 1))
+        for i in range(0, shape[0] + 1):
+            expected_frequencies[i][0] = 0
+        for i in range(0, shape[1] + 1):
+            expected_frequencies[0][i] = 0
+
+        self.assertTrue(np.array_equal(expected_frequencies, frequencies))
+
+    def test_sample_bounds(
+        self,
+        gsd: float = 1,
+        radius: float = 1,
+        padding: float = 0,
+        processes: int = 1,
+    ):
+        shape = (10, 10)
+        x_arr = np.arange(0.5, shape[1], 1)
+        y_arr = np.arange(0.5, shape[0], 1)
+        x, y = np.meshgrid(x_arr, y_arr)
+        values = np.ones(len(x_arr) * len(y_arr))
+
+        sample_bounds = shapely.geometry.Polygon(
+            [(3, 3), (3, 7), (7, 7), (7, 3), (3, 3)]
+        )
+
+        affine_transform, frequencies, out_avg, _ = hm.generate_density_heatmap(
+            x.ravel(),
+            y.ravel(),
+            values,
+            gsd=gsd,
+            radius=radius,
+            padding=padding,
+            processes=processes,
+            sample_bounds=sample_bounds,
+        )
+
+        expected_result = np.ones((6, 6))
+        expected_result[0][0] = 0
+        expected_result[5][5] = 0
+        self.assertTrue(np.array_equal(out_avg, expected_result))
+
+        expected_bounds = (
+            math.floor(3 - radius),
+            math.ceil(3 - radius),
+            math.ceil(7 + radius),
+            math.ceil(7 + radius),
+        )
+        self.assertEqual(
+            expected_bounds,
+            windows.bounds(windows.get_data_window(out_avg), affine_transform),
+        )
+
+        self.assertTrue(np.array_equal(expected_result, frequencies))
+
+    def test_frequency_reuse(
+        self,
+        gsd: float = 1,
+        radius: float = 1,
+        padding: float = 0,
+        processes: int = 1,
+    ):
+        shape = (10, 10)
+
+        x_arr = np.arange(0.5, shape[1], 1)
+        y_arr = np.arange(0.5, shape[0], 1)
+        x, y = np.meshgrid(x_arr, y_arr)
+        values = np.ones(len(x_arr) * len(y_arr))
+        (
+            affine_transform,
+            frequencies,
+            out_avg,
+            raw_frequencies,
+        ) = hm.generate_density_heatmap(
+            x.ravel(),
+            y.ravel(),
+            values,
+            gsd=gsd,
+            radius=radius,
+            padding=padding,
+            processes=processes,
+        )
+
+        affine_transform_2, frequencies_2, out_avg_2, _ = hm.generate_density_heatmap(
+            x.ravel(),
+            y.ravel(),
+            values,
+            gsd=gsd,
+            radius=radius,
+            padding=padding,
+            processes=processes,
+            frequencies=raw_frequencies,
+        )
+
+        self.assertTrue(np.array_equal(frequencies, frequencies_2))
+        self.assertTrue(np.array_equal(out_avg, out_avg_2))
+        self.assertEqual(affine_transform, affine_transform_2)
+
+    def test_single_location(
+        self,
+        gsd: float = 1,
+        radius: float = 1,
+        padding: float = 0,
+        processes: int = 1,
+    ):
+        shape = (10, 10)
+        x_arr = np.ones(shape[1])
+        y_arr = np.ones(shape[0])
+        x, y = np.meshgrid(x_arr, y_arr)
+        values = np.ones(len(x_arr) * len(y_arr))
+        affine_transform, frequencies, out_avg, _ = hm.generate_density_heatmap(
+            x.ravel(),
+            y.ravel(),
+            values,
+            gsd=gsd,
+            radius=radius,
+            padding=padding,
+            processes=processes,
+        )
+
+        expected_result = np.ones((2, 2))
+        self.assertTrue(np.array_equal(out_avg, expected_result))
+
+        expected_bounds = (
+            math.floor(1 - radius),
+            math.ceil(1 - radius),
+            math.ceil(1 + radius),
+            math.ceil(1 + radius),
+        )
+        self.assertEqual(
+            expected_bounds,
+            windows.bounds(windows.get_data_window(out_avg), affine_transform),
+        )
+
+        expected_frequencies = np.full((2, 2), shape[0] * shape[1])
+
+        self.assertTrue(np.array_equal(expected_frequencies, frequencies))
