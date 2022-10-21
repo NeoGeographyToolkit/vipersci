be8e0a58cd87bd34176513a94adee7681190ea1d
Ross Beyer <rbeyer@rossbeyer.net>
2022-10-21 08:29:02 -0700

    feat(datetime.py): Now supports the sep and timespec arguments of datetime.datetime.isoformat() and includes a comprehensive docstring.

--- src/vipersci/pds/datetime.py
+++ src/vipersci/pds/datetime.py
@@ -28,9 +28,29 @@ ISO 8601 handling to deal with trailing "Z" characters instead of +0.
 import datetime
 
 
-def isozformat(date_time):
+def isozformat(date_time, sep="T", timespec="auto"):
+    """
+    Return a string representing the UTC date and time in ISO 8601 format with
+    the trailing letter 'Z' representing the UTC offset for the provided
+    UTC *date_time* object.
+
+    This function is similar to the standard library's
+    datetime.datetime.isoformat() function (see that documentation
+    for details on *sep* and *timespec*).
+
+    The difference is that the provided *date_time* must be timezone
+    aware and it must be in UTC (its utcoffset() must not be None and
+    must equal zero). Otherwise, it will raise a ValueError.
+
+    The most important difference is that rather than returning a string
+    representation that ends in "+00:00" for a UTC datetime object, it
+    will return a representation that ends in "Z".  Both formats are
+    valid ISO 8601 date formats, but the standard library picks one
+    string representation, and this function provides the other, which
+    is the required format for PDS4 labels.
+    """
     if date_time.utcoffset() == datetime.timedelta():
-        return date_time.replace(tzinfo=None).isoformat() + "Z"
+        return date_time.replace(tzinfo=None).isoformat(sep, timespec) + "Z"
     else:
         raise ValueError(
             "The datetime object is either naive (not timezone aware), "
