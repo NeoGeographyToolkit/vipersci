5b67a5e5604653401a1158c79b1a5e2b768488ae
Ross Beyer <rbeyer@rossbeyer.net>
2023-07-31 15:33:47 -0700

    fix(header.py): Updated pga_gain_dict so that the keys are integers and the values are all floats, which is relevant for type checking.

--- src/vipersci/vis/header.py
+++ src/vipersci/vis/header.py
@@ -37,11 +37,11 @@ pga_gain_dict = {
     # The key is the integer value of the 3-bit parameter from the Image
     # Header.  The value is the floating point value that corresponds
     # to the 3-bit parameter.
-    0: 1,
+    0: 1.0,
     1: 1.2,
     2: 1.4,
     3: 1.6,
-    4: 2,  # 4, 5, 6, & 7 are 2x 0, 1, 2, 3
+    4: 2.0,  # 4, 5, 6, & 7 are 2x 0, 1, 2, 3
     5: 2.4,
     6: 2.8,
     7: 3.2,
